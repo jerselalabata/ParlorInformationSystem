ef3ecbcee79d3f7a78e3eb297eaaf467184cef66
jerselalabata <jersel.macalisang@gmail.com>
2019-01-18 07:00:15 +0800

    add content

---
+++ products.php
@@ -0,0 +1,98 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<title>Salon de Bro</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="bootstrap-4.2.1/css/bootstrap.min.css">
+<!--<link rel="stylesheet" type="text/css" href="style.css">-->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<body>
+<style type="text/css">
+.navbar-logo {
+  width: 90px;
+}
+</style>
+
+<!-- !PAGE CONTENT! -->
+<!-- Header -->
+<div class="container-fluid p-0">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar10">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse collapse" id="navbar10">
+            <ul class="navbar-nav nav-fill w-100">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Welcome</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Products</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">News</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+</div>
+<div class="container-fluid p-0">
+  <img src="Shelton.png" class="img-fluid w-100" alt="Responsive image">
+</div>
+
+  
+<!-- End Page Content -->
+<!-- Footer -->
+
+<div class="container-fluid p-0">
+<div class="jumbotron text-center">
+<div class="container-fluid p-0">
+</div>
+  <div class="row">
+  <form>
+  <div class="form-row">
+    <div class="form-group">
+      <label for="">Name</label>
+      <input type="text" class="form-control" id="name" placeholder="Name">
+    </div>
+    <div class="form-group">
+      <label for="">Last Name</label>
+      <input type="text" class="form-control" id="lname" placeholder="Last Name">
+    </div>
+  </div>
+  <div class="form-group>
+      <label for="inputEmail4">Email</label>
+      <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+  </div><br>
+  <div class="col-auto col-md-14">
+      <button type="submit" class="btn btn-primary mb-2">Submit</button>
+  </div>
+</form>
+    <div class="col sm-4">
+      <footer class="w3-container w3-padding-64 w3-light-grey w3-center w3-large"> 
+        <i class="fa fa-facebook-official w6-hover-opacity"></i>
+        <i class="fa fa-instagram w6-hover-opacity"></i>
+        <i class="fa fa-snapchat w6-hover-opacity"></i>
+        <i class="fa fa-pinterest-p w6-hover-opacity"></i>
+        <i class="fa fa-twitter w6-hover-opacity"></i>
+        <i class="fa fa-linkedin w6-hover-opacity"></i>
+    </div>
+        <div class="col sm-4">.col</div>
+      </footer>
+
+      <!-- <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank" class="w3-hover-text-green">Linux</a></p> -->
+</div>
+</div>
+</div>
+</body>
+</html>
