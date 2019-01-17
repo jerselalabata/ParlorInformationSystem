8b7e4ad17c709ec6a313725dd0db9a2384afb836
jerselalabata <jersel.macalisang@gmail.com>
2019-01-17 20:36:14 +0800

    partial2 commit

--- index.php
+++ index.php
@@ -5,40 +5,56 @@
 <title>Salon de Bro</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="bootstrap-4.1.3/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="bootstrap-4.2.1/css/bootstrap.min.css">
+<!--<link rel="stylesheet" type="text/css" href="style.css">-->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<style>
-body,h1 {font-family: "Raleway", Arial, sans-serif}
-h1 {letter-spacing: 6px}
-.w3-row-padding img {margin-bottom: 12px}
-</style>
 <body>
+<style type="text/css">
+.navbar-logo {
+  width: 90px;
+}
+</style>
 
 <!-- !PAGE CONTENT! -->
-<div class="w3-content" style="max-width:1500px">
-
 <!-- Header -->
-<header class="w3-panel w3-center w3-opacity" style="padding:128px 16px">
-  <h1 class="w3-xlarge">Salon De Bro</h1>
-  
-  <div class="w3-padding-32">
-    <div class="w3-bar w3-border">
-      <a href="#" class="w3-bar-item w3-button">Welcome</a>
-      <a href="#" class="w3-bar-item w3-button w3-light-grey">Services</a>
-      <a href="#" class="w3-bar-item w3-button">Products</a>
-      <a href="#" class="w3-bar-item w3-button w3-hide-small">Contact Us</a>
-      <a href="#" class="w3-bar-item w3-button w3-hide-small">About Us</a>
-
+<div class="container-fluid p-0">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar10">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse collapse" id="navbar10">
+            <ul class="navbar-nav nav-fill w-100">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Welcome</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Products</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">News</a>
+                </li>
+            </ul>
+        </div>
     </div>
-  </div>
-</header>
-  
-<!-- End Page Content -->
+</nav>
+</div>
+</nav>
+<div class="container-fluid p-0">
+  <img src="Shelton.png" class="img-fluid w-100" alt="Responsive image">
 </div>
 
+  
+<!-- End Page Content -->
 <!-- Footer -->
+<div class="container-fluid p-0">
 <footer class="w3-container w3-padding-64 w3-light-grey w3-center w3-large"> 
   <i class="fa fa-facebook-official w3-hover-opacity"></i>
   <i class="fa fa-instagram w3-hover-opacity"></i>
@@ -48,6 +64,6 @@ h1 {letter-spacing: 6px}
   <i class="fa fa-linkedin w3-hover-opacity"></i>
   <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank" class="w3-hover-text-green">Linux</a></p>
 </footer>
-
+</div>
 </body>
 </html>
