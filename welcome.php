2b38e374fc3179e200d554af91433490e31e7561
jerselalabata <jersel.macalisang@gmail.com>
2019-01-21 14:08:12 +0800

    add pages

--- welcome.php
+++ welcome.php
@@ -44,5 +44,7 @@
 <script src="bootstrap-4.0.0/dist/js/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="bootstrap-4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </ul>
+
 </body>
+
 </html>
