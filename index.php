b58814ae23231107d5e67234580f56eaf0fbb5b5
jerselalabata <jersel.macalisang@gmail.com>
2019-01-21 13:44:24 +0800

    fixed navbar

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 <!DOCTYPE html>
 <html>
 <title>Salon de Bro</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="bootstrap-4.2.1/css/bootstrap.min.css">
-<!--<link rel="stylesheet" type="text/css" href="style.css">-->
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="bootstrap-4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
 <body>
 <style type="text/css">
 .navbar-logo {
@@ -41,6 +41,20 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#">News</a>
                 </li>
+                <li class="nav-item">
+                    <a href="index.php?logout='1'" class="btn" type="button" style="color: white;">Log out</a>
+                </li>
+                <div class="dropdown">
+                  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Menu
+                  </button>
+                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">View Eployee</a>
+                    <a class="dropdown-item" href="#">View Customer</a>
+                    <a class="dropdown-item" href="#">View Services</a>
+                    <a class="dropdown-item" href="#">View Tools</a>
+                  </div>
+                </div>
             </ul>
         </div>
     </div>
@@ -49,48 +63,27 @@
 <div class="container-fluid p-0">
   <img src="Shelton.png" class="img-fluid w-100" alt="Responsive image">
 </div>
-
-  
 <!-- End Page Content -->
 <!-- Footer -->
-
-<div class="container-fluid p-0">
-<div class="jumbotron text-center">
-<div class="container-fluid p-0">
-</div>
-  <div class="row">
-  <form>
-  <div class="form-row">
-    <div class="form-group">
-      <label for="">Name</label>
-      <input type="text" class="form-control" id="name" placeholder="Name">
-    </div>
-    <div class="form-group">
-      <label for="">Last Name</label>
-      <input type="text" class="form-control" id="lname" placeholder="Last Name">
-    </div>
+<footer class="w3-container w3-center w3-opacity w3-margin-bottom">
+  <h5>Find Us On</h5>
+  <div class="w3-xlarge w3-padding-16">
+    <i class="fa fa-facebook-official w3-hover-opacity"></i>
+    <i class="fa fa-instagram w3-hover-opacity"></i>
+    <i class="fa fa-snapchat w3-hover-opacity"></i>
+    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+    <i class="fa fa-twitter w3-hover-opacity"></i>
+    <i class="fa fa-linkedin w3-hover-opacity"></i>
   </div>
-  <div class="form-group>
-      <label for="inputEmail4">Email</label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
-  </div><br>
-  <div class="col-auto col-md-14">
-      <button type="submit" class="btn btn-primary mb-2">Submit</button>
-  </div>
-</form>
-    <div class="col sm-4">
-      <footer class="w3-container w3-padding-64 w3-light-grey w3-center w3-large"> 
-        <i class="fa fa-facebook-official w6-hover-opacity"></i>
-        <i class="fa fa-instagram w6-hover-opacity"></i>
-        <i class="fa fa-snapchat w6-hover-opacity"></i>
-        <i class="fa fa-pinterest-p w6-hover-opacity"></i>
-        <i class="fa fa-twitter w6-hover-opacity"></i>
-        <i class="fa fa-linkedin w6-hover-opacity"></i>
-    </div>
-        <div class="col sm-4">.col</div>
+</footer>
+        <div class="col sm-6">
+          <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank" class="w3-hover-text-green">Linux</a></p>
+        </div>
       </footer>
-
-      <!-- <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank" class="w3-hover-text-green">Linux</a></p> -->
+<script src="bootstrap-4.0.0/dist/js/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="bootstrap-4.0.0/dist/js/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="bootstrap-4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+      
 </div>
 </div>
 </div>
---
+++ welcome.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="bootstrap-4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Solon de Bro</title>
+</head>
+<body>
+<ul class="nav nav-pills">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Active</a>
+  </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="#">Action</a>
+      <a class="dropdown-item" href="#">Another action</a>
+      <a class="dropdown-item" href="#">Something else here</a>
+      <div class="dropdown-divider"></div>
+      <a class="dropdown-item" href="#">Separated link</a>
+    </div>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#">Disabled</a>
+  </li>
+  <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Dropdown button
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">Action</a>
+    <a class="dropdown-item" href="#">Another action</a>
+    <a class="dropdown-item" href="#">Something else here</a>
+  </div>
+</div>
+<script src="bootstrap-4.0.0/dist/js/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="bootstrap-4.0.0/dist/js/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="bootstrap-4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</ul>
+</body>
+</html>
